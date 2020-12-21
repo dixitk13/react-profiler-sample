a0771bfc50833f32df70f32ca5ca17492892449d
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2020-12-21 12:17:52 -0800

    notes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,11 +5,16 @@ import { reducer, initState } from "./reducer";
 export const App = () => {
   const [state, dispatch] = React.useReducer(reducer, initState);
 
+  // this causes it somehow? two cases try using
+  // 1. L9-L13 + L37 (uncomment L40)
+  // 2. L40 directly (uncomment step 1 lines)
+
   const index = 0;
   const components = [
     <RechartList datapoints={state?.datapoints} streams={state?.streams} />,
   ];
   const MyComponent = () => components[index];
+
   return (
     <React.Profiler
       id="app"
@@ -22,14 +27,6 @@ export const App = () => {
           alignItems: "flex-start",
         }}
       >
-        {/* <input
-          type="number"
-          value={state?.charts}
-          placeholder="enter charts"
-          onChange={(e) =>
-            dispatch({ type: "UPDATE_CHARTS", charts: e.target.value })
-          }
-        ></input> */}
         <input
           type="number"
           value={state?.datapoints}
@@ -38,22 +35,9 @@ export const App = () => {
             dispatch({ type: "UPDATE_DATAPOINTS", datapoints: e.target.value })
           }
         ></input>
-        {/* <input
-          type="number"
-          value={state?.streams}
-          placeholder="enter streams"
-          onChange={(e) =>
-            dispatch({ type: "UPDATE_STREAMS", streams: e.target.value })
-          }
-        ></input> */}
         <hr style={{ margin: "30px 0" }} />
-
-        {/* {Array(Number(state?.charts))
-          .fill(0)
-          .map((_, idx) => (
-            
-          ))} */}
         <MyComponent />
+        {/* <RechartList datapoints={state?.datapoints} streams={state?.streams} /> */}
       </div>
     </React.Profiler>
   );
